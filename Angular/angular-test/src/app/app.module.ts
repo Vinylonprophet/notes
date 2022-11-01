0dbb39ad31c77d68d0daf70a9d4facde90346f90
Vinylonprophet <915390118@qq.com>
2022-11-02 01:08:20 +0800

    Angular Study

--- Angular/angular-test/src/app/app.module.ts
+++ Angular/angular-test/src/app/app.module.ts
@@ -1,18 +1,29 @@
+/*
+ * @Author: Vinylonprophet 915390118@qq.com
+ * @Date: 2022-11-02 00:57:36
+ * @LastEditors: Vinylonprophet 915390118@qq.com
+ * @LastEditTime: 2022-11-02 01:01:06
+ * @FilePath: \angular-test\src\app\app.module.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+  // 声明当前模块拥有哪些组件
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  // 声明当前模块依赖了那些其他模块
+    BrowserModule
   ],
+  // 声明服务的作用域，数组中接受服务类，表示该服务只能在当前模块的组件中使用
   providers: [],
+  // 可引导组件，Angular 会在引导过程中把它加载到 DOM 中
+  // 当前的 AppComponent 是作为启动组件
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular/angular-test/src/main.ts
+++ Angular/angular-test/src/main.ts
@@ -1,10 +1,23 @@
+/*
+ * @Author: Vinylonprophet 915390118@qq.com
+ * @Date: 2022-11-02 00:57:36
+ * @LastEditors: Vinylonprophet 915390118@qq.com
+ * @LastEditTime: 2022-11-02 01:00:29
+ * @FilePath: \angular-test\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// 通过调用这个方法开启生产模式
 import { enableProdMode } from '@angular/core';
+// angular在不同平台上的启动不同，现在是在浏览器上启动，该方法返回平台实例对象
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+// 引入根模块 用于启动应用程序
 import { AppModule } from './app/app.module';
+// 引入环境变量对象 { production: false }
 import { environment } from './environments/environment';
 
+// 如果当前为生产环境，则开启生产模式
 if (environment.production) {
+  // 开启生产模式
   enableProdMode();
 }
 
