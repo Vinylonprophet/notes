e64d0a6195a5347f72dd37c221f4fa0366db8c4c
Vinylonprophet <915390118@qq.com>
2022-11-24 21:45:41 +0800

    angular

--- Angular/angular-test/src/app/ngrx-example-one/ngrx-example-one.component.ts
+++ Angular/angular-test/src/app/ngrx-example-one/ngrx-example-one.component.ts
@@ -2,7 +2,7 @@
  * @Author: Vinylonprophet 915390118@qq.com
  * @Date: 2022-11-24 17:09:47
  * @LastEditors: Vinylonprophet 915390118@qq.com
- * @LastEditTime: 2022-11-24 18:56:30
+ * @LastEditTime: 2022-11-24 21:43:51
  * @FilePath: \angular-test\src\app\ngrx-example-one\ngrx-example-one.component.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
--- Angular/angular-test/src/app/store/reducers/todo.reducer.ts
+++ Angular/angular-test/src/app/store/reducers/todo.reducer.ts
@@ -2,10 +2,11 @@
  * @Author: Vinylonprophet 915390118@qq.com
  * @Date: 2022-11-24 17:17:32
  * @LastEditors: Vinylonprophet 915390118@qq.com
- * @LastEditTime: 2022-11-24 17:47:39
+ * @LastEditTime: 2022-11-24 21:42:10
  * @FilePath: \angular-test\src\app\store\reducers\todo.reducer.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 import { Action, createReducer, on } from '@ngrx/store';
 import { retry } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
@@ -19,30 +20,16 @@ export interface Todo {
   title: string
 }
 
-export interface State {
-  todos: Todo[]
-}
+export interface State extends EntityState<Todo>{}
 
-export const initialState: State = {
-  todos: []
-};
+export const adapter: EntityAdapter<Todo> = createEntityAdapter<Todo>()
+
+export const initialState: State = adapter.getInitialState()
+
+console.log(initialState)
 
 export const reducer = createReducer(
   initialState,
-  on(addTodo, (state, action) => ({
-    ...state,
-    todos:[
-      ...state.todos,
-      {
-        id: uuidv4(),
-        title: action.title
-      }
-    ]
-  })),
-  on(deleteTodo, (state, action) => {
-    const newState: State = JSON.parse(JSON.stringify(state))
-    const index = newState.todos.findIndex(todo => todo.id === action.id)
-    newState.todos.splice(index, 1)
-    return newState
-  })
+  on(addTodo, (state, action) => adapter.addOne({id: uuidv4(), title: action.title}, state)),
+  on(deleteTodo, (state, action) => adapter.removeOne(action.id, state))
 );
