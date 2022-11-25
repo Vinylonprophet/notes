c75104b2104046f124914a31a21aaeea2d0345c1
Vinylonprophet <915390118@qq.com>
2022-11-25 15:13:53 +0800

    angular

--- Angular/angular-test/src/app/animations/demo/demo.component.ts
+++ Angular/angular-test/src/app/animations/demo/demo.component.ts
@@ -2,11 +2,11 @@
  * @Author: Vinylonprophet 915390118@qq.com
  * @Date: 2022-11-25 14:28:23
  * @LastEditors: Vinylonprophet 915390118@qq.com
- * @LastEditTime: 2022-11-25 14:53:34
+ * @LastEditTime: 2022-11-25 15:13:24
  * @FilePath: \angular-test\src\app\animations\demo\demo.component.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, style, transition, trigger, AnimationEvent } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -25,9 +25,18 @@ import { Component, OnInit } from '@angular/core';
         animate(250)
       ]),
       // 出场动画
+      // transition("* => void", [
+      //   // 运行总时间 延迟时间（可选） 运动形式（可选）
+      //   animate("600ms 1s ease-out", style({ opacity: 0, transform: "translateX(100%)" }))
+      // ]),
       transition("* => void", [
-        // 运行总时间 延迟时间（可选） 运动形式（可选）
-        animate("600ms 1s ease-out", style({ opacity: 0, transform: "translateX(100%)" }))
+        animate(
+          "600ms 1s ease-out",
+          keyframes([
+            style({ offset: 0.3, transform: "translateX(-80px)" }),
+            style({ offset: 1, transform: "translateX(100%)" }),
+          ])
+        )
       ]),
     ])
   ]
@@ -41,14 +50,24 @@ export class DemoComponent implements OnInit {
 
   // todo 列表
   todos: string[] = ["Learn Angular", "Learn RxJS", "Learn NgRx"]
+
   // 添加 todo
   addItem(input: HTMLInputElement) {
     this.todos.push(input.value)
     input.value = ""
   }
   // 删除 todo
+
   removeItem(index: number) {
     this.todos.splice(index, 1)
+  }
+
+  startSlide(event: AnimationEvent) {
+    console.log(event);
+  }
+
+  doneSlide(event: AnimationEvent) {
+    console.log(event);
   }
 
 }
